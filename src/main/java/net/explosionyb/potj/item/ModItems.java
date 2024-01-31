f52c4eb7c555142745aeae5a0f49469e2721d3d4
ExplosionYB <explosion.yb@gmail.com>
2024-01-31 17:01:43 +0100

    Ilum Ice (fix required)

--- src/main/java/net/explosionyb/potj/item/ModItems.java
+++ src/main/java/net/explosionyb/potj/item/ModItems.java
@@ -2,9 +2,7 @@ package net.explosionyb.potj.item;
 
 import net.explosionyb.potj.PathoftheJedi;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
